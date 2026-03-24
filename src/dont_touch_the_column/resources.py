dadf802aa0e27bfb1a720530b553277af0fe4b8c
Claudia Tassi <claudiatassi2010@gmail.com>
2026-03-24 09:02:45 +0100

    modifica del path delle immagini (funziona)

--- src/dont_touch_the_column/resources.py
+++ src/dont_touch_the_column/resources.py
@@ -1,8 +1,11 @@
 from importlib.resources import files
 from pathlib import Path
 
+BASE_DIR = Path(__file__).parent
+
 def get_image(filename: str) -> Path:
-    return files(__package__) / "images" / filename
+    return BASE_DIR / "images" / filename
 
 def get_json(filename: str) -> Path:
-    return files(__package__) / "file_json" / filename
+    return BASE_DIR / "file_json" / filename
+
